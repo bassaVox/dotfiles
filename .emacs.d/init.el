5d9156c7be62da29a101908ffac025be71ee6059
damenipotu <damenipotu@gmail.com>
2019-10-18 23:15:37 -0300

    Emacs configuration in org mode.

---
+++ .emacs.d/init.el
@@ -0,0 +1,19 @@
+(package-initialize)
+;; on emacs 27
+;; package-refresh-contents
+
+(org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (dap-mode company-box company-lsp lsp-ui lsp-mode company helm-projectile general which-key use-package magit helm doom-themes))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
