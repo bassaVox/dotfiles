6539e4e91f76c49d9a37bf16776a2e0353c5ad8b
damenipotu <damenipotu@gmail.com>
2020-01-22 21:55:44 -0300

    Emacs - sml mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,7 +9,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(elpy flycheck-rust cargo rust-mode toml-mode flycheck dap-mode company-box company-lsp lsp-ui lsp-mode company helm-projectile general which-key use-package magit helm doom-themes)))
+   (quote
+    (elpy flycheck-rust cargo rust-mode toml-mode flycheck dap-mode company-box company-lsp lsp-ui lsp-mode company helm-projectile general which-key use-package magit helm doom-themes))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
